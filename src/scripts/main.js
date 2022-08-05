2e1b09689105a7045f3a9c4441fb9e9c0ce8d284
Victor <victorsmirnov@me.com>
2022-08-05 16:56:14 +0300

    add burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,50 @@ new Gallery(document.getElementById('gallery'), {
   margin: 10,
 });
 
+// Toggle nav menu active
+const toggleMenu = item => {
+  const links = document.querySelectorAll(item);
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      links.forEach(link => link.classList.remove('active'));
+      link.classList.add('active');
+    });
+  });
+};
+
+// Burger menu support
+const body = document.querySelector('body'), 
+      cover = document.querySelector('.space-cover'), 
+      menu = document.querySelector('.burger-menu'), 
+      control = document.querySelector('.burger-menu__control'),
+      burger = document.querySelector('.burger__item'),
+      close = document.querySelector('.close__item'),
+      links = document.querySelectorAll('.main-nav__link'),
+      
+      BODY_BURGER_OPENED = 'body-burger_opened',
+      MENU_OPENED_CLASS = 'burger-menu__opened';
+
+const openMenu = () => {
+  body.classList.add(BODY_BURGER_OPENED);
+  menu.classList.add(MENU_OPENED_CLASS);
+  burger.style.display = 'none';
+  close.style.display = 'block';
+};
+
+const closeMenu = () => {
+  body.classList.remove(BODY_BURGER_OPENED);
+  menu.classList.remove(MENU_OPENED_CLASS);
+  burger.style.display = 'block';
+  close.style.display = 'none';
+};
+
+const burgerMenu = () => {
+  cover.addEventListener('click', () => closeMenu());
+  control.addEventListener('click', () => menu.classList.contains(MENU_OPENED_CLASS) ? closeMenu() : openMenu());
+  links.forEach(link => link.addEventListener('click', () => closeMenu()));
+};
+
+// Initial functions
+toggleMenu('.nav-link');
+burgerMenu();
 functions.isWebp();
